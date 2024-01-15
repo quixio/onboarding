58e622d4ab39c560de9dea2ce7f6258ee28e81d2
Tomas Neubauer <tomas@quix.io>
2024-01-15 16:01:12 +0000

    Edited 'main.py'

--- Demo Data/main.py
+++ Demo Data/main.py
@@ -37,7 +37,7 @@ with Producer(
         # Create a dictionary that includes both column headers and row values
         row_data = {header: row[header] for header in headers}
 
-        row_data["Timestamp"] = time.time_ns()
+        row_data["Timestamp"] = time.time()
 
         producer.produce(
             topic=topic,
