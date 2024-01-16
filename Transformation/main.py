edfce381a7b6a7315a68fb77f55d3f140314b4a6
Tomas Neubauer <tomas@quix.io>
2024-01-16 14:20:50 +0000

    Edited 'main.py'

--- Transformation/main.py
+++ Transformation/main.py
@@ -3,36 +3,14 @@ from quixstreams import Application, State
 
 app = Application.Quix("transformation-v8", auto_offset_reset="latest")
 
-def get_timestamp(val: dict, *_):
-    print("HAHA")
-    return val["Timestamp"] / 1000000
-
-input_topic = app.topic(os.environ["input"], value_deserializer='json', timestamp_extractor=get_timestamp)
+input_topic = app.topic(os.environ["input"], value_deserializer='json')
 output_topic = app.topic(os.environ["output"], value_serializer='json')
 
 sdf = app.dataframe(input_topic)
-sdf = sdf[sdf.contains("Speed") & sdf["Speed"].notnull()]
-
-def reduce_speed(state: dict, val: float) -> dict:
-    state["last"] = val
-    state["min"] = min(state["min"], val) 
-    state["max"] = max(state["max"], val) 
-
-    return state
-
-def init_reduce_speed(val: float) -> dict:
-    
-    return {
-        "last" : val,
-        "first": val,
-        "max": val,
-        "min": val
-    }
-sdf = sdf.apply(lambda val: val["Speed"]).tumbling_window(10, 0).reduce(reduce_speed, init_reduce_speed).final()
-
-sdf = sdf.update(lambda row: print(row))
 
 #sdf = sdf.to_topic(output_topic)
+
+sdf = sdf.update(lambda row: print(row))
 
 if __name__ == "__main__":
     app.run(sdf)
