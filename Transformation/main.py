df011e2b7f2821da27bcd3b46156c49f2a2a63e4
Tomas Neubauer <tomas@quix.ai>
2024-02-29 18:57:01 +0100

    Modified using Git CLI

--- Transformation/main.py
+++ Transformation/main.py
@@ -1,12 +1,28 @@
 import os
 from quixstreams import Application, State
+import uuid
+from datetime import datetime
 
-app = Application.Quix("transformation-v8", auto_offset_reset="latest")
+app = Application.Quix(str(uuid.uuid4()), auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"], value_deserializer='json')
 output_topic = app.topic(os.environ["output"], value_serializer='json')
 
 sdf = app.dataframe(input_topic)
+
+sdf = sdf[["Timestamp", "Brake"]]
+sdf = sdf[sdf["Brake"].notnull()]
+
+sdf = sdf.apply(lambda row: row["Brake"]).hopping_window(1000, 200).mean().final()
+
+sdf = sdf[sdf["value"] > 0.5]
+
+sdf = sdf.apply(lambda row: {
+    "Timestamp": row["start"],
+    "Alert": {
+        "Message": "Hard braking detected"
+    }
+})
 
 #sdf = sdf.to_topic(output_topic)
 
